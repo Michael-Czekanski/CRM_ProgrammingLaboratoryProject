92f596d273e2967099caa7d17080444bf993e36a
Michał Czekański <michal.cz2202@gmail.com>
2019-12-14 20:52:33 +0100

    Add database context

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Database/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using CustomerRelationshipManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.Database
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach(var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<BusinessNote> BusinessNotes { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ContactPerson> ContactPeople { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+    
+}
