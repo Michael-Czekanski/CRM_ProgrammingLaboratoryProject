8226c0af28a7d3771bcc69afacda78a5725b3923
Michał Czekański <michal.cz2202@gmail.com>
2019-12-20 19:44:28 +0100

    Create companies controller with only constructor

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Controllers/CompaniesController.cs
@@ -0,0 +1,22 @@
+﻿using CustomerRelationshipManager.DataRepositories;
+using CustomerRelationshipManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.Controllers
+{
+    public class CompaniesController: Controller
+    {
+        private IDataRepository<Company> _companyRepository;
+
+        public CompaniesController(IDataRepository<Company> companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+    }
+    
+}
