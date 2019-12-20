81f68ae812764b9ec5749c027c5fef260ef9ec8b
Michał Czekański <michal.cz2202@gmail.com>
2019-12-20 19:30:09 +0100

    Add company repository interface
    This repository inherits from IDataRepository<Company>.
    Added it, because there is need to fill navigation properties of company
    objects.

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/DataRepositories/ICompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using CustomerRelationshipManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.DataRepositories
+{
+    interface ICompanyRepository: IDataRepository<Company>
+    {
+        Company FillBusinessIndustryNavProperty(Company companyToFillWithData);
+
+        Company FillContactPeopleNavProperty(Company companyToFillWithData);
+
+        Company FillBusinessNotesNavProperty(Company companyToFillWithData);
+    }
+}
