484a781ecba74808e11d424dbf1f6781e7d0a712
Michał Czekański <michal.cz2202@gmail.com>
2019-12-22 02:33:59 +0100

    Add helper which hashes password

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Helpers/UserManagementHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CustomerRelationshipManager.Helpers
+{
+    public static class UserManagementHelper
+    {
+        public static string HashPasswordSHA256(string passwd)
+        {
+            return Encoding.UTF8.GetString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(passwd)));
+        }
+
+        
+    }
+}
