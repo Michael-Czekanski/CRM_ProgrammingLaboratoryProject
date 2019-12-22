29a3ca9240c00643e870181f7773a376e5442688
Michał Czekański <michal.cz2202@gmail.com>
2019-12-22 02:37:51 +0100

    Configure request processing pipeline to require authentication from users

--- CustomerRelationshipManager/CustomerRelationshipManager/Controllers/HomeController.cs
+++ CustomerRelationshipManager/CustomerRelationshipManager/Controllers/HomeController.cs
@@ -6,21 +6,55 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CustomerRelationshipManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using CustomerRelationshipManager.ViewModels;
+using CustomerRelationshipManager.DataRepositories;
+using System.Text;
+using CustomerRelationshipManager.Helpers;
 
 namespace CustomerRelationshipManager.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IUserRepository _userRepository;
+        private readonly ITokenProvider tokenProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUserRepository userRepository, ITokenProvider tokenProvider)
         {
             _logger = logger;
+            this._userRepository = userRepository;
+            this.tokenProvider = tokenProvider;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult SignIn(UserSignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = _userRepository.Authenticate(model.Login, model.Password);
+                if (user == null)
+                {
+                    return BadRequest(new { message = "Username or password is incorrect" });
+                }
+                string token = tokenProvider.ProvideToken(user);
+                HttpContext.Session.Set("JWT", Encoding.ASCII.GetBytes(token));
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
--- CustomerRelationshipManager/CustomerRelationshipManager/Startup.cs
+++ CustomerRelationshipManager/CustomerRelationshipManager/Startup.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CustomerRelationshipManager.Database;
 using CustomerRelationshipManager.DataRepositories;
+using CustomerRelationshipManager.Helpers;
 using CustomerRelationshipManager.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +15,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CustomerRelationshipManager
 {
@@ -27,6 +31,8 @@ namespace CustomerRelationshipManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
+
             services.AddControllersWithViews();
             services.AddDbContextPool<AppDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("CRMDbConnection")));
@@ -36,6 +42,30 @@ namespace CustomerRelationshipManager
             services.AddScoped<IDataRepository<BusinessNote>, SQLBusinessNoteRepository>();
             services.AddScoped<ICompanyRepository, SQLCompanyRepository>();
             services.AddScoped<IDataRepository<ContactPerson>, SQLContactPersonRepository>();
+            services.AddSingleton<ITokenProvider, TokenProvider>();
+
+            IConfiguration appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            AppSettings appSettings = appSettingsSection.Get<AppSettings>();
+            byte[] secretKey = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(token =>
+            {
+                token.RequireHttpsMetadata = false;
+                token.SaveToken = true;
+                token.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,11 +81,31 @@ namespace CustomerRelationshipManager
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseSession();
+
+            // Filling header with token
+            app.Use(async (context, next) =>
+            {
+                byte[] JWTBytes;
+                if (context.Session.TryGetValue("JWT", out JWTBytes))
+                {
+                    string JWT = Encoding.ASCII.GetString(JWTBytes);
+                    if (JWT != null)
+                    {
+                        context.Request.Headers.Add("Authorization", "Bearer " + JWT);
+                    }
+                }
+                await next();
+            });
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
