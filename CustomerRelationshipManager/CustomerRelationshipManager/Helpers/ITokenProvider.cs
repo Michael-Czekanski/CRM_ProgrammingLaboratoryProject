fdbf04f3787d94f49d51a0d42bae927ad7ea66de
Michał Czekański <michal.cz2202@gmail.com>
2019-12-22 02:36:54 +0100

    Add token provider which creates tokens for user

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Helpers/ITokenProvider.cs
@@ -0,0 +1,13 @@
+﻿using CustomerRelationshipManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.Helpers
+{
+    public interface ITokenProvider
+    {
+        string ProvideToken(User user);
+    }
+}
---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Helpers/TokenProvider.cs
@@ -0,0 +1,41 @@
+﻿using CustomerRelationshipManager.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.Helpers
+{
+    public class TokenProvider: ITokenProvider
+    {
+        private readonly AppSettings appSettings;
+
+        public TokenProvider(IOptions<AppSettings> appSettings)
+        {
+            this.appSettings = appSettings.Value;
+        }
+
+        public string ProvideToken(User user)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.ID.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
