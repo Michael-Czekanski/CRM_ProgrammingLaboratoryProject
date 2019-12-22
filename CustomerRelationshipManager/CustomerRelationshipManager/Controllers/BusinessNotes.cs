f3d681c8d2580f33e0287f3c1c4491f9e4eed3a5
Michał Czekański <michal.cz2202@gmail.com>
2019-12-22 19:01:21 +0100

    Add business notes controller with just constructor

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Controllers/BusinessNotes.cs
@@ -0,0 +1,20 @@
+﻿using CustomerRelationshipManager.DataRepositories;
+using CustomerRelationshipManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.Controllers
+{
+    public class BusinessNotes: Controller
+    {
+        private readonly IBusinessNoteRepository _businessNoteRepository;
+
+        public BusinessNotes(IBusinessNoteRepository businessNoteRepository)
+        {
+            _businessNoteRepository = businessNoteRepository;
+        }
+    }
+}
