e23e920b67430e987b1a7fba52a438671f5e7955
Michał Czekański <michal.cz2202@gmail.com>
2019-12-22 21:50:43 +0100

    Add contact people controller

---
+++ CustomerRelationshipManager/CustomerRelationshipManager/Controllers/ContactPeopleController.cs
@@ -0,0 +1,17 @@
+﻿using CustomerRelationshipManager.DataRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerRelationshipManager.Controllers
+{
+    public class ContactPeopleController
+    {
+        private readonly IContactPersonRepository _contactPersonRepository;
+        public ContactPeopleController(IContactPersonRepository contactPersonRepository)
+        {
+            _contactPersonRepository = contactPersonRepository;
+        }
+    }
+}
