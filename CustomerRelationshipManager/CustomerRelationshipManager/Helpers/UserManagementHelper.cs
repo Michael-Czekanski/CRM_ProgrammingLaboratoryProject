7857535237a702c436273d00e5acc52529854a8b
Michał Czekański <michal.cz2202@gmail.com>
2019-12-25 20:25:43 +0100

    Add method which tries to retrieve logged in user id
    In UserManagementHelperClass.

--- CustomerRelationshipManager/CustomerRelationshipManager/Helpers/UserManagementHelper.cs
+++ CustomerRelationshipManager/CustomerRelationshipManager/Helpers/UserManagementHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace CustomerRelationshipManager.Helpers
@@ -10,6 +12,22 @@ namespace CustomerRelationshipManager.Helpers
             return Encoding.UTF8.GetString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(passwd)));
         }
 
+        public static bool TryGetLoggedUserID(HttpContext context, out int varToPutID)
+        {
+            byte[] loggedInUserIDBytes;
+            int loggedInUserID;
+            if (context.Session.TryGetValue("UserID", out loggedInUserIDBytes))
+            {
+                loggedInUserID = BitConverter.ToInt32(loggedInUserIDBytes, 0);
+                varToPutID = loggedInUserID;
+                return true;
+            }
+            else
+            {
+                varToPutID = -1;
+                return false;
+            }
+        }
         
     }
 }
